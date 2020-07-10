e13fdb68ad2aa1a1bd55e7134a118f6751e819a6
Aleksey <luksiko90@gmail.com>
2020-07-10 11:35:48 +0500

    Second day 2

--- script/script.js
+++ script/script.js
@@ -1,44 +1,67 @@
 'use strict';
+
+const dataBase = [];
+
 const modalAdd = document.querySelector('.modal__add'),
 	modalItem = document.querySelector('.modal__item'),
 	addAd = document.querySelector('.add__ad'),
 	modalBtnSubmit = document.querySelector('.modal__btn-submit'),
 	modalSubmit = document.querySelector('.modal__submit'),
-	catalog = document.querySelector('.catalog');
+	catalog = document.querySelector('.catalog'),
+	modalBtnWarning = document.querySelector('.modal__btn-warning');
+
+const elementsModalSubmit = [...modalSubmit.elements].filter((elem) => elem.tagName !== 'BUTTON');
+
+
+const escape = (event) => {
+	if (event.key === 'Escape') {
+		modalItem.classList.add('hide');
+		modalAdd.classList.add('hide');
+		document.removeEventListener('keydown', escape);
+	}
+};
+
+const closeModal = function (event) {
+	const target = event.target;
+	if (target.closest('.modal__close') || target === this) {
+		this.classList.add('hide');
+		if (this === modalAdd) {
+			modalSubmit.reset();
+		}
+	}
+};
+// Проверяем на введенные символы и разблокируем кнопку если введено
+modalSubmit.addEventListener('input', () => {
+	const validForms = elementsModalSubmit.every((elem) => elem.value);
+	modalBtnSubmit.disabled = !validForms;
+	modalBtnWarning.style.display = validForms ? 'none' : '';
+});
+modalSubmit.addEventListener('submit', (event) => {
+	event.preventDefault();
+	// создаем обьявления
+	const itemObj = {};
+		for (const elem of elementsModalSubmit) {
+			itemObj[elem.name] = elem.value;
+		}
+	dataBase.push(itemObj);
+	modalSubmit.reset();
+	console.log(dataBase);
+});
 
 addAd.addEventListener('click', () => {
 	modalAdd.classList.remove('hide');
 	modalBtnSubmit.disabled = true;
+	document.addEventListener('keydown', escape);
 });
+
+modalItem.addEventListener('click', closeModal);
+modalAdd.addEventListener('click', closeModal);
 
 catalog.addEventListener('click', (event) => {
 	const target = event.target;
-	if (target.closest('li') || target === modalItem) {
-		console.log(target);
+	if (target.closest('.card') || target === modalItem) {
 		modalItem.classList.remove('hide');
 		modalSubmit.reset();
+		document.addEventListener('keydown', escape);
 	}
 });
-
-function close(item) {
-	item.addEventListener('click', (event) => {
-		const target = event.target;
-		if (target.classList.contains('modal__close') || target === item) {
-			item.classList.add('hide');
-			modalSubmit.reset();
-		}
-	});
-}
-
-function escape(item) {
-	document.addEventListener('keydown', (event) => {
-		if (event.key === 'Escape') {
-			item.classList.add('hide');
-		}
-	});
-}
-
-close(modalItem);
-close(modalAdd);
-escape(modalItem);
-escape(modalAdd);
